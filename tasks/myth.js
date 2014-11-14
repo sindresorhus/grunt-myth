bcf475b52042c8c72257c1fbda301655ad42c052
Bruno Sabot <brunosabot@gmail.com>
2014-11-14 12:31:07 +0100

    Enable recursive myth on folders

--- tasks/myth.js
+++ tasks/myth.js
@@ -3,13 +3,41 @@ var eachAsync = require('each-async');
 var myth = require('myth');
 
 module.exports = function (grunt) {
-	grunt.registerMultiTask('myth', 'Postprocess CSS with Myth', function () {
-		var options = this.options();
 
-		eachAsync(this.files, function (el, i, next) {
-			options.source = el.src[0];
-			grunt.file.write(el.dest, myth(grunt.file.read(el.src[0]), options));
+	grunt.registerMultiTask('myth', 'Postprocess CSS with Myth', function () {
+
+		var options = this.options();
+		var execute = function ( dest, src, options ) {
+			grunt.file.write( dest, myth( grunt.file.read( src ), options ) );
+		};
+
+		eachAsync( this.files, function ( el, i, next ) {
+
+			var localOptions = options;
+
+			localOptions.source = el.src[ 0 ];
+
+			if ( el.src[0][ el.src[0].length - 1 ] === '/' ) {
+
+				grunt.file.recurse( el.src[0], function callback( abspath, rootdir, subdir, filename ) {
+
+					var localDest = el.dest + filename;
+					var localSrc = abspath;
+
+					localOptions.source = localSrc;
+
+					execute( localDest, localSrc, localOptions );
+
+				} );
+
+			} else {
+				execute( el.dest, el.src[ 0 ], localOptions );
+			}
+
 			next();
+
 		}, this.async());
+
 	});
+
 };
